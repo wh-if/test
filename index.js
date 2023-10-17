1ca98632ca791fd54fc128844557df86b61b1530
wh <884923319@qq.com>
2023-10-17 10:43:07 +0800

    11

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const http = require("http")
 
 const fs = require('fs/promises')
-fs.writeFile('./t1.txt', "hello t1");
-// let flag = false;
-// const server = http.createServer(async (req, res) => {
-//   if (!false) {
-//     flag = true;
-//   }
-//   const result = await fs.readdir('.');
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   res.end(JSON.stringify({
-//     data: result,
-//   }));
-// });
 
-// server.listen(8000);
+let flag = false;
+const server = http.createServer(async (req, res) => {
+  if (!false) {
+    fs.writeFile('./t1.txt', "hello t1");
+    flag = true;
+  }
+  const result = await fs.readdir('.');
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: result,
+  }));
+});
+
+server.listen(8000);
